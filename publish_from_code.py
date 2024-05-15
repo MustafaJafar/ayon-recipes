8689db30687a9fdc91335146d737b02ce2839971
MustafaJafar <mustafataherzaky@outlook.com>
2024-05-15 18:22:05 +0300

    make publish from generic

--- publish_from_code.py
+++ publish_from_code.py
@@ -2,40 +2,38 @@
 How to trigger publish programmatically using pyblish api.
 """
 
-import hou
+"""
+How to trigger publish programmatically using pyblish api.
+"""
+
 import logging
 import pyblish
 from ayon_core.pipeline import registered_host
 from ayon_core.pipeline.create import CreateContext
 
-logging.basicConfig()
-
-log = logging.getLogger("publish-from-code")
-log.setLevel(logging.DEBUG)
-
 host = registered_host()
 
-inputs_nodes = [
-    hou.node('/out/pointcacheBgeoExample').path()
-]
+assert host is None, "No registered host."
 
-comment = "publish from code"
-context = CreateContext(host, reset=True)
-log.debug("\nNodes to publish: {}\n".format(inputs_nodes))
-for instance in context.instances:
-    node_path = instance.data.get("instance_node")
-    instance["active"] = node_path and node_path in inputs_nodes
+logging.basicConfig()
+log = logging.getLogger("publish-from-code")
+log.setLevel(logging.DEBUG)
+    
+create_context = CreateContext(host)
 
-context.save_changes()
+# # filter instances based on some condition
+# for instance in create_context.instances:
+#     if (some condition):
+#         instance["active"] = False
+#     instance["active"] = True
+# context.save_changes()
 
 pyblish_context = pyblish.api.Context()
-pyblish_context.data["create_context"] = context
-pyblish_context.data["comment"] = comment
-
-pyblish_plugins = context.publish_plugins
+pyblish_context.data["create_context"] = create_context
+pyblish_context.data["comment"] = "publish from code"
+pyblish_plugins = create_context.publish_plugins
 
 error_format = "Failed {plugin.__name__}: {error} -- {error.traceback}"
-
 for result in pyblish.util.publish_iter(pyblish_context, pyblish_plugins):
     for record in result["records"]:
         log.debug("{}: {}".format(result["plugin"].label, record.msg))
