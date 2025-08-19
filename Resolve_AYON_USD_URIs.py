f48c61b16efe41d438851c29543b270fad72791a
MustafaJafar <mustafataherzaky@outlook.com>
2025-08-19 20:36:17 +0300

    Resolve_AYON_USD_URIs.py: Add `latest` version and info about special versions in AYON.

--- Resolve_AYON_USD_URIs.py
+++ Resolve_AYON_USD_URIs.py
@@ -3,6 +3,8 @@ Accessing the USD Resolver Manually to Resolve a URI Path
 
 This example demonstrates how to manually access the USD Resolver to resolve a URI path.
 While the `resolver.Resolve()` method is generic, this specific example is for an AYON context and resolves an AYON Entity URI.
+
+For reference, the AYON Server supports two special versions: `HERO` and `latest`.
 """
 
 from pxr import Ar
@@ -12,7 +14,7 @@ resolver = Ar.GetResolver()
 project_name = "Experiments"
 folder_path = "/things/deadline_submissions"
 product = "pointcacheTestBgeo"
-version = "v009" # "HERO"
+version = "latest" # "v008" # "HERO"
 representation = "bgeo.sc"
 
 unresolved_path = f"ayon+entity://{project_name}{folder_path}?product={product}&version={version}&representation={representation}"
