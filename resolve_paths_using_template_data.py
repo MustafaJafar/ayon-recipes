ef2ca04b00e40d9bd25af443975898e02d4d6462
MustafaJafar <mustafataherzaky@outlook.com>
2025-09-29 15:20:45 +0300

    Update `resolve_paths_using_template_data.py`

--- resolve_paths_using_template_data.py
+++ resolve_paths_using_template_data.py
@@ -1,29 +1,26 @@
 """
 Get Template data and resolve work directory template.
 """
-import ayon_api
-from ayon_core.pipeline import (
-    Anatomy,
-    get_current_context,
-    get_current_host_name,
-)
-from ayon_core.pipeline.template_data import get_template_data
 from ayon_core.lib import StringTemplate
+from ayon_core.pipeline import Anatomy
+from ayon_core.pipeline.template_data import get_template_data
+from ayon_core.pipeline.context_tools import (
+    get_current_project_entity,
+    get_current_folder_entity,
+    get_current_task_entity,
+    get_current_host_name
+)
+
+from ayon_core.pipeline.workfile import get_workdir
 
 
-# Get Current Context
+# Get Current host name
 host_name = get_current_host_name()
-context = get_current_context()
-project_name = context["project_name"]
-folder_path = context["folder_path"]
-task_name = context["task_name"]
 
 # Get Entities
-project_entity = ayon_api.get_project(project_name)
-folder_entity = ayon_api.get_folder_by_path(project_name, folder_path)
-task_entity = ayon_api.get_task_by_name(
-    project_name, folder_entity["id"], task_name
-)
+project_entity = get_current_project_entity()
+folder_entity = get_current_folder_entity()
+task_entity = get_current_task_entity()
 
 # Get Template Data
 template_data = get_template_data(
@@ -31,7 +28,7 @@ template_data = get_template_data(
 )
 
 # Add Roots to template data
-anatomy = Anatomy(project_name, project_entity=project_entity)
+anatomy = Anatomy()
 template_data["root"] = anatomy.roots
 
 # Resolve Path 
@@ -39,13 +36,10 @@ work_dir = anatomy.templates["work"]["default"]["directory"]
 work_dir = StringTemplate.format_template(work_dir, template_data)
 print(work_dir)
 
-
 # -------------------------------
 
 # Alternatively, for this particular example,
 #   we already have a function to retrieve resolved work dir
-from ayon_core.pipeline.workfile import get_workdir
-
 
 work_dir = get_workdir(
     project_entity, folder_entity, task_entity, host_name
