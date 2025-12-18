be89caad3cf7748d6531d74ea1acb9975b644d40
MustafaJafar <mustafataherzaky@outlook.com>
2025-12-18 12:48:09 +0200

    Add `set_addon_settings.py`

---
+++ set_addon_settings.py
@@ -0,0 +1,49 @@
+"""
+This snippet is for applying changes in settings for a project. 
+
+The code flow is as follows: 
+1. Get project settings.
+2. Change the value of desired settings.
+3. Set project settings.
+
+Note:
+    you can use it to modify the studio settings 
+    by removing the "/{project_name}"
+
+    To get further info about Sending Custom Requests, see 
+    https://docs.ayon.dev/docs/dev_api_python/#sending-custom-requests
+"""
+
+import ayon_api
+from ayon_api.utils import prepare_query_string
+
+
+# Specify Addon and project name
+addon_name = "core"
+version = "1.6.13+dev"
+project_name = "Experiments"
+variant = "AYON-Dev"
+site_id = None
+
+# Prepare endpoint
+query = prepare_query_string({
+    "site": site_id,
+    "variant": variant,
+})
+entrypoint = f"addons/{addon_name}/{version}/settings/{project_name}{query}"
+
+
+# Get Settings
+response = ayon_api.get(entrypoint)
+settings = response.data
+
+# Modify Settings
+settings["publish"]["IntegrateHeroVersion"] = False
+
+# Set Settings
+response = ayon_api.post(
+    entrypoint,
+    **settings
+)
+
+print(response.detail)
